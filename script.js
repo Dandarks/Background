5558d7534aef27240664ac936758329873425775
Danilo <61158290+Dandarks@users.noreply.github.com>
2022-02-10 21:09:17 -0300

    Files

---
+++ script.js
@@ -0,0 +1,26 @@
+function buttonClick (color){
+    bg = document.getElementById("corpo");
+    bg.style.backgroundColor = color;
+    bg.style.transition = "background-color 0.7s";
+}
+
+
+function clique () {
+    document.getElementById("button");
+    bg = document.getElementById("corpo");
+    bg.style.backgroundColor = "black";
+}
+
+function changeColor () {
+    color = document.getElementById("color").value;
+    bg = document.getElementById("corpo");
+    bg.style.backgroundColor = color;
+    bg.style.transition = "background-color 0.7s";
+}
+
+function changeBg () {
+    color = document.getElementById("color").value;
+    bg = document.getElementById("corpo");
+    bg.style.backgroundColor = color;
+    bg.style.transition = "background-color 0.7s";
+}
