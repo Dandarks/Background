46fb6143a654b93000bfadaac9b198d06f19380c
Danilo <61158290+Dandarks@users.noreply.github.com>
2022-02-11 16:53:08 -0300

    Clear

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-function buttonClick (color){
-    bg = document.getElementById("corpo");
-    bg.style.backgroundColor = color;
-    bg.style.transition = "background-color 0.7s";
-}
+var bC = document.getElementById("corpo").style.transition = "background-color 1.0s";
 
+function buttonClick (color){
+    bg = document.getElementById("corpo").style.backgroundColor = color;
+    bC;
+}
 
 function clique () {
     document.getElementById("button");
-    bg = document.getElementById("corpo");
-    bg.style.backgroundColor = "black";
+    bg = document.getElementById("corpo").style.backgroundColor = "black";
+    bC;
 }
 
 function changeColor () {
     color = document.getElementById("color").value;
-    bg = document.getElementById("corpo");
-    bg.style.backgroundColor = color;
-    bg.style.transition = "background-color 0.7s";
+    bg = document.getElementById("corpo").style.backgroundColor = color;
+    bC;
 }
 
 function changeBg () {
     color = document.getElementById("color").value;
-    bg = document.getElementById("corpo");
-    bg.style.backgroundColor = color;
-    bg.style.transition = "background-color 0.7s";
+    bg = document.getElementById("corpo").style.backgroundColor = color;
+    bC;
 }
